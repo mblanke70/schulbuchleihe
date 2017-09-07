5aa6f9d31ed4a8628f4af3a8ca8f49b11a233545
Malte Blanke <malte.blanke@web.de>
2017-09-07 09:34:09 +0200

    oauth client routes

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,35 @@ Route::resource('buchlisten', 'BooklistController');
  | Bücher
  */
 Route::resource('buecher', 'BookController');
+
+
+
+
+
+Route::get('/redirect', function () {
+
+    $query = http_build_query([
+        'client_id' => '54_7tavq619x98g4oks4k4w0w80g8kssc0os4kw4oowcwwgcwc00',
+        'redirect_uri' => 'http://localhost/schulbuchleihe/callback',
+        'response_type' => 'code',
+        'scope' => ''
+    ]);
+
+    return redirect('https://urs-os.de/iserv/oauth/v2/auth?'.$query);
+});
+
+
+Route::get('/callback', function (Illuminate\Http\Request $request) {
+    $http = new \GuzzleHttp\Client;
+
+    $response = $http->post('https://urs-os.de/iserv/oauth/v2/token', [
+        'form_params' => [
+            'client_id' => '54_7tavq619x98g4oks4k4w0w80g8kssc0os4kw4oowcwwgcwc00',
+            'client_secret' => '55c64yrtnlogooss0ogcocgg4w8g4wgs48oo8wk4c0wog4k0gs',
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => 'http://localhost/schulbuchleihe/callback',
+            'code' => $request->code,
+        ],
+    ]);
+    return json_decode((string) $response->getBody(), true);
+});
